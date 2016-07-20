1a7c6c0da79066f4ab12f01d9016b4240e70dcdc
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-19 22:17:58 -0300

    Exercício 7, Módulo 2

--- Programa.java
+++ Programa.java
@@ -2,9 +2,9 @@ class Programa {
 
     public static void main(String[] args) {
         
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 40; i++) {
             
-            int fatorial = 1;
+            long fatorial = 1;
             for (int j = i; j >= 1; j--) {
                 fatorial *= j;
             }
