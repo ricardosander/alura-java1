722bf6556e15e58ea49441440eb42c99ac8743d3
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-19 22:39:03 -0300

    Exercício 4, Módulo 3

--- Programa.java
+++ Programa.java
@@ -1,25 +1,19 @@
 class Programa {
     
     public static void main(String[] args) {
-        
-        Funcionario f1 = new Funcionario();
-        f1.nome = "Primeiro funcionário";
-        f1.departamento = "RH";
+
+    	Funcionario f1 = new Funcionario();        
+        f1.nome = "Ricardo";
         f1.salario = 100;
-        f1.dataEntrada = "28/12/2000";
-        f1.rg = "123";
-        
-        Funcionario f2 = new Funcionario();
-        f2.nome = "Segundo funcionário";
-        f2.departamento = "Atendimento";
-        f2.salario = 50;
-        f2.dataEntrada = "12/03/2005";
-        f2.rg = "345";
-        
-        f1.recebeAumento(100);
-        f2.recebeAumento(75);
-        
-        f1.mostra();
-        f2.mostra();
+
+        Funcionario f2 = new Funcionario();        
+        f2.nome = "Ricardo";
+        f2.salario = 100;
+
+        if (f1 == f2) {
+            System.out.println("São iguais");
+        } else {
+            System.out.println("São diferentes");        
+        }
     }
 }
