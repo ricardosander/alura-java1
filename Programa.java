4ed9da4019caccfdfb55b9fe903500bae7030fd1
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-19 22:12:29 -0300

    Exercício 3, Módulo 2

---
+++ Programa.java
@@ -0,0 +1,11 @@
+class Programa {
+	
+    public static void main(String[] args) {
+        int cont = 150;
+        while (cont <= 300) {
+            System.out.println(cont);
+            cont++;
+        }
+        
+    }
+}
