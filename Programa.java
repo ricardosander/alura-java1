427a1591170445877f35a0d6862189bfe1e92be8
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-19 22:18:51 -0300

    Exercício 8, Módulo 2

--- Programa.java
+++ Programa.java
@@ -2,13 +2,16 @@ class Programa {
 
     public static void main(String[] args) {
         
-        for (int i = 1; i <= 40; i++) {
+        int numero = 13;
+        System.out.print(13);
+        while (numero != 1) {
             
-            long fatorial = 1;
-            for (int j = i; j >= 1; j--) {
-                fatorial *= j;
+            if (numero % 2 == 0) {
+                numero = numero / 2;
+            } else {
+                numero = 3 * numero + 1;
             }
-            System.out.println(i + "! = " + fatorial);
+            System.out.print(" > " + numero);
         }
     }
 }
