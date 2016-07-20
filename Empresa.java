e86b21dfab64725d320dd2cfdb08f731cc571d86
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-19 22:50:37 -0300

    Exercício 2, Módulo 4

--- Empresa.java
+++ Empresa.java
@@ -4,4 +4,15 @@ class Empresa {
     String cnpj;
     Funcionario[] funcionarios;
 
+    void adiciona(Funcionario funcionario) {
+
+    	label: for (int i = 0; i < this.funcionarios.length; i++) {
+
+    		if (this.funcionarios[i] == null) {
+
+    			this.funcionarios[i] = funcionario;
+                break label;
+            }
+        }
+    }
 }
