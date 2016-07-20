6edf39af129818528593b7958304d36b70121b46
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-19 22:15:02 -0300

    Exercício 5, Módulo 2

--- Programa.java
+++ Programa.java
@@ -2,10 +2,9 @@ class Programa {
 
     public static void main(String[] args) {
         
-        int soma = 0;
-        for (int i = 1; i <= 1000; i++) {
-            soma += i;
+        for (int i = 3; i <= 100; i = i + 3) {
+        	System.out.println( i + " é divisível por 3.");
+        	
         }
-        System.out.println("A soma de 1 até 1000 é " + soma);
     }
 }
